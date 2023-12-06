eee71f0819ab5ecea3dd8d1d882e03a47c48b9ae
Laila Case <lai_case@hotmail.com>
2023-12-06 14:59:20 +0000

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import AllMobiles from "./components/AllMobiles";
+import logo from "./assets/logo.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <img className="logo" src={logo} alt="logo" />
+      <h1>Sell-a-Phone</h1>
       <AllMobiles />
     </>
   );
