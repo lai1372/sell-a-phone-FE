cabb0b7b08d58dbd26f2251c4afbb5b01c05ba9a
Laila Case <lai_case@hotmail.com>
2023-12-11 10:46:14 +0000

    add filtering by search box

--- src/components/AllMobiles.jsx
+++ src/components/AllMobiles.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function AllMobiles() {
   const [mobiles, setMobiles] = useState([]);
+  const [filteredResults, setFilteredResults] = useState(mobiles);
+  const [searchInput, setSearchInput] = useState("");
   const navigate = useNavigate();
 
   async function fetchMobiles() {
@@ -12,11 +14,19 @@ export default function AllMobiles() {
       const response = await fetch("http://localhost:8080/mobiles");
       const mobileData = await response.json();
       setMobiles(mobileData);
-      console.log(mobileData);
     } catch (err) {
       return err;
     }
   }
+  const handleChange = (e) => {
+    const results = mobiles.filter(
+      (mobile) =>
+        mobile.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+        mobile.brand.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setFilteredResults(results);
+    setSearchInput(e.target.value);
+  };
 
   function handleClick(mobile) {
     navigate(`/mobiles/${mobile.id}`);
@@ -27,20 +37,51 @@ export default function AllMobiles() {
   }, []);
   return (
     <>
-      {mobiles.map((mobile, i) => (
-        <div
-          className="mobile-container"
-          key={i}
-          onClick={() => {
-            handleClick(mobile);
-          }}
-        >
-          <img className="list-image" src={mobile.image} />
-          <p>{mobile.name}</p>
-          <p>{mobile.brand}</p>
-          <p>£{mobile.price}</p>
+      <input
+        type="text"
+        placeholder="Search for a mobile name or brand"
+        onChange={handleChange}
+        value={searchInput}
+      />
+      {searchInput.length === 0 ? (
+        <div>
+          <div>
+            {mobiles.map((mobile, idx) => (
+              <div
+                key={idx}
+                onClick={() => {
+                  handleClick(mobile);
+                }}
+              >
+                <img className="list-image" src={mobile.image} />
+                <p>{mobile.name}</p>
+                <p>{mobile.brand}</p>
+                <p>£{mobile.price}</p>
+                <br />
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
+      ) : (
+        <div>
+          <div>
+            {filteredResults.map((mobile, idx) => (
+              <div
+                key={idx}
+                onClick={() => {
+                  handleClick(mobile);
+                }}
+              >
+                <img className="list-image" src={mobile.image} />
+                <p>{mobile.name}</p>
+                <p>{mobile.brand}</p>
+                <p>£{mobile.price}</p>
+                <br />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </>
   );
 }
